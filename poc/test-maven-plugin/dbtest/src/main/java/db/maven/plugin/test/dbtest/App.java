0ca41752657de83bf580faf201d4bb35e807a794
valarcon <valarcon>
2019-12-06 14:30:02 -0500

    Adding test-project.

---
+++ poc/test-maven-plugin/dbtest/src/main/java/db/maven/plugin/test/dbtest/App.java
@@ -0,0 +1,13 @@
+package db.maven.plugin.test.dbtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ poc/test-maven-plugin/dbtest/testdata/dbsrc/1.0.0/build.sql
@@ -0,0 +1,13 @@
+create table port (
+  id int primary key not null,
+  name varchar(20) not null
+);
+
+create table ship (
+  id int primary key not null,
+  name varchar(20) not null,
+  legal_location int not null,
+  constraint fk_ship_port foreign key (legal_location) references port(id) 
+);
+
+
