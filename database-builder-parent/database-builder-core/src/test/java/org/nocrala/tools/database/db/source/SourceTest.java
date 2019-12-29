1895b76254038435e7008a0b282c7d7a593b7034
valarcon <valarcon@example.com>
2019-12-29 13:24:16 -0500

    Modularised for Maven, Ant, Gradle.

---
+++ database-builder-parent/database-builder-core/src/test/java/org/nocrala/tools/database/db/source/SourceTest.java
@@ -0,0 +1,113 @@
+package org.nocrala.tools.database.db.source;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.nocrala.tools.database.db.ConfigurationProperties;
+import org.nocrala.tools.database.db.executor.Delimiter;
+import org.nocrala.tools.database.db.executor.SQLExecutor;
+import org.nocrala.tools.database.db.executor.SQLExecutor.CouldNotConnectToDatabaseException;
+import org.nocrala.tools.database.db.executor.SQLExecutor.CouldNotReadSQLScriptException;
+import org.nocrala.tools.database.db.executor.SQLExecutor.InvalidPropertiesFileException;
+import org.nocrala.tools.database.db.executor.SQLExecutor.SQLScriptAbortedException;
+import org.nocrala.tools.database.db.source.Source.InvalidDatabaseSourceException;
+import org.nocrala.tools.database.db.version.VersionNumber;
+
+public class SourceTest {
+
+  @Test
+  public void testMain() {
+
+    // performTest();
+
+  }
+
+  private void performTest() {
+    // Parameters
+
+    String databaseDir = "testdata/dbsrc";
+    String version = "1.1.0";
+    String scenario = "dev1";
+
+    boolean layeredBuild = true;
+    boolean layeredScenarios = false;
+    boolean buildOnErrorContinue = false;
+    boolean cleanOnErrorContinue = true;
+
+    String localdatabaseproperties = "testdata/localdatabase.properties";
+
+    Delimiter del = new Delimiter(";", false, false);
+
+    // Execution
+
+    TestFeedback feedback = new TestFeedback();
+
+    File dbdir = new File(databaseDir);
+    File propsFile = new File(localdatabaseproperties);
+    VersionNumber currentVersion = new VersionNumber(version);
+
+    Source s;
+    try {
+      ConfigurationProperties config = null;
+      s = new Source(config, feedback);
+    } catch (InvalidDatabaseSourceException e1) {
+      // TODO Auto-generated catch block
+      e1.printStackTrace();
+      return;
+    }
+
+    SQLExecutor sqlExecutor = null;
+
+    try {
+      ConfigurationProperties config = null;
+      sqlExecutor = new SQLExecutor(config, feedback);
+    } catch (InvalidPropertiesFileException e) {
+      System.out.println("Invalid properties file: " + renderException(e));
+      // e.printStackTrace();
+      return;
+    } catch (CouldNotConnectToDatabaseException e) {
+      System.out.println("Could not connect to database: " + renderException(e));
+      // e.printStackTrace();
+      return;
+    }
+
+    try {
+
+      s.build(currentVersion, sqlExecutor);
+      // s.clean(currentVersion, sqlExecutor, feedback );
+      // s.rebuild(currentVersion, scenario, sqlExecutor, feedback );
+
+    } catch (CouldNotReadSQLScriptException e) {
+      // System.out.println("Could not read SQL script file.");
+      // System.out.println(" -> " + renderException(e));
+      // e.printStackTrace();
+      return;
+    } catch (SQLScriptAbortedException e) {
+      // System.out.println("SQL statement failed.");
+      // System.out.println(" -> " + renderException(e));
+      // e.printStackTrace();
+      return;
+    }
+  }
+
+  private String renderException(final Throwable e) {
+    StringBuilder sb = new StringBuilder();
+    Throwable t = e;
+    boolean first = true;
+    while (t != null) {
+      String m = t.getMessage();
+      if (m != null && !m.trim().isEmpty()) {
+        if (first) {
+          first = false;
+        } else {
+          sb.append(": ");
+        }
+        sb.append(m.trim());
+      }
+      t = t.getCause();
+    }
+    return sb.toString();
+
+  }
+
+}
