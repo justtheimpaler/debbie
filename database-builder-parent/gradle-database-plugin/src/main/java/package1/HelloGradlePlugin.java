0eb1b4f44a6a84c617a6745ba38132a86418a4a8
valarcon <valarcon@example.com>
2019-12-29 16:20:57 -0500

    Initial Gradle coding.

---
+++ database-builder-parent/gradle-database-plugin/src/main/java/package1/HelloGradlePlugin.java
@@ -0,0 +1,14 @@
+package package1;
+
+
+public class HelloGradlePlugin
+//implements Plugin<Project>
+{
+
+//  @Override
+//  public void apply(Project project) {
+//    project.getExtensions().create("demoSetting", DemoPluginExtension.class);
+//    project.getTasks().create("demo", DemoTask.class);
+//  }
+
+}
