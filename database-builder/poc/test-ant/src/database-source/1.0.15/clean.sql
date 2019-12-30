2fe5ef37414203b0420321949ecb5f47b8a68056
valarcon <valarcon>
2019-12-30 14:59:54 -0500

    Added ant test project.

---
+++ database-builder/poc/test-ant/src/database-source/1.0.15/clean.sql
@@ -0,0 +1,3 @@
+alter table port drop column manager_id;
+
+drop table port_manager;
---
+++ database-builder/poc/test-ant/src/database-source/1.2/dev1/build-data.sql
@@ -0,0 +1,6 @@
+
+insert into port (id, name) values (101, 'Chicago');
+insert into port (id, name) values (102, 'Valparaiso');
+
+insert into country (id, name) values (12, 'Kenia');
+
